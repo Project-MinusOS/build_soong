4644ea00bec1a3d3834bcf4f79b2d250de8ed14f
SkyMinus <minus_rav@qq.com>
2025-02-11 23:22:18 +0800

    Add vendor/minus to kotlin plugins neverallow
    We don't want to fork prebuilts/sdk just for that
    
    Change-Id: I5f6ff87e341d2391354b1379291de92318599eeb
    (cherry picked from commit 681549f2a09613ab33abe57d7ee851066c53c324)

--- android/neverallow.go
+++ android/neverallow.go
@@ -368,6 +368,7 @@ func createKotlinPluginRule() []Rule {
 	kotlinPluginProjectsAllowedList := []string{
 		"external/kotlinc",
 		"tools/strictdeps/kotlin",
+		"vendor/minus/kotlin",
 	}
 
 	return []Rule{
